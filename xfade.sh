2f3fbf62ee571755f6ceff825227824cab17c57f
xukainan <xukainan@fang.com>
2020-12-02 15:40:32 +0800

    xfade-支持视频

--- xfade.sh
+++ xfade.sh
@@ -10,10 +10,13 @@ ulimit -c 9999999
 #OFFSET_STRS="6.5"
 #test 多个
 METHOD_STRS="fade slideleft"
-IMAGE_STRS="002.jpg 003.jpg 004.jpg"
+IMAGE_STRS="002.jpg 003.jpg 26.mp4"
 TIME_STRS="6 7 8"
 DURATION_STRS="2 2"
 OFFSET_STRS="4 7"
+FORMAT_STRS="1 1 2" # 格式数组，1为图片 2为视频
+OUT_WIDTH="640"
+OUT_HEIGHT="360"
 # 传参
 #METHOD_STRS=$1
 #IMAGE_STRS=$2
@@ -22,11 +25,13 @@ OFFSET_STRS="4 7"
 #OFFSET_STRS=$5
 
 
+OUT_SIZE=$OUT_WIDTH":"$OUT_HEIGHT
 METHOD_ARRAY=($METHOD_STRS)
 IMAGE_ARRAY=($IMAGE_STRS)
 DURATION_ARRAY=($DURATION_STRS)
 OFFSET_ARRAY=($OFFSET_STRS)
 TIME_ARRAY=($TIME_STRS)
+FORMAT_ARRAY=($FORMAT_STRS)
 
 IMAGE_ARRAY_LENGTH=${#IMAGE_ARRAY[@]}
 echo $IMAGE_ARRAY_LENGTH
@@ -44,6 +49,12 @@ if [ $IMAGE_ARRAY_LENGTH -eq 2 ];
 else
     $(> temp)
     tempstr=""
+    index_scale=0
+    while [ $index_scale -lt $IMAGE_ARRAY_LENGTH ]
+    do
+        echo -n "["$index_scale":v]scale=640:360["$index_scale"]," >> temp
+        index_scale=$(($index_scale+1))
+    done
     index=0
     for method in $METHOD_STRS;
     do
@@ -82,8 +93,15 @@ cycleStr=""
 index=0
 for image in $IMAGE_STRS;
 do
- echo $cycleStr " -loop 1 -t "${TIME_ARRAY[$index]}" -i " $image >> temp
- let "index+=1"
+ if [ ${FORMAT_ARRAY[$index]} -eq 1 ];
+ then
+      echo $cycleStr " -r 24 -loop 1 -t "${TIME_ARRAY[$index]}" -i " $image >> temp
+      let "index+=1"
+ else
+      echo $cycleStr " -r 24 -i " $image >> temp
+      let "index+=1"
+ fi
+
 done
 
 echo "=========h2========"
